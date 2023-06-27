fedebc3b7237a38663649ad4bae291de3a0aa66e
lamiyahell <axira1108@gmail.com>
2023-06-27 19:12:31 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+
+    public static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    public static List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+
+    public static void main(String[] args) {
+
+    }
+}
